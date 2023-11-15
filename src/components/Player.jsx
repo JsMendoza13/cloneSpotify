7c8a9e35c75bb691d598c112146c2d1680529170
Jesus Mendoza Martinez <jsmendoza1312@gmail.com>
2023-11-15 16:42:07 -0500

    volume control finish

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -77,14 +77,33 @@ const CurrentSong = ({ image, title, artists }) => {
 const VolumeControl = () => {
   const volume = usePlayerStore((state) => state.volume);
   const setVolume = usePlayerStore((state) => state.setVolume);
+  const previousVolumeRef = useRef(volume);
+
+  const isVolumeSilenced = volume < 0.1;
+
+  const handleClickVolumen = () => {
+    if (isVolumeSilenced) {
+      setVolume(previousVolumeRef.current);
+    } else {
+      previousVolumeRef.current = volume;
+      setVolume(0);
+    }
+  };
 
   return (
     <div className="flex justify-center gap-x-2 text-white">
-      {volume < 0.1 ? <VolumeSilence /> : <Volume />}
+      <button
+        className="opacity-70 hover:opacity-100 transition"
+        onClick={handleClickVolumen}
+      >
+        {isVolumeSilenced ? <VolumeSilence /> : <Volume />}
+      </button>
+
       <Slider
         defaultValue={[100]}
         max={100}
         min={0}
+        value={[volume * 100]}
         className="w-[95px]"
         onValueChange={(value) => {
           const [newVolume] = value;
